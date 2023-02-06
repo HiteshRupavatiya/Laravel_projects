7863abc43981ba1153d1aeba34937c1981237c67
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-06 19:31:26 +0530

    EmployeeContoller Added and Index.blade file added and perform query builder operation

---
+++ query_builder_demo/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return DB::table('employees')->get(); // Returns All Data Of Table In Array
+
+        // $data = DB::table('employees')->where('id',4)->get(); // Returns Data Of The Id 4 Whole Record
+
+        // $data = DB::table('employees')->where('email','zconroy@example.net')->get(); // it returns the specified email record in table
+
+        // return (array) DB::table('employees')->find(4); // it returns an array of id 4 whole record
+
+        // return DB::table('employees')->count(); // It returns total record count of table
+
+        return DB::table('employees')->insert([
+            'name' => 'abc',
+            'email' => 'abc@gmail.com',
+            'address' => 'amreli'
+        ]);
+
+        // return view('index',['data' => $data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
